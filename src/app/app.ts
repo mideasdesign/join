65d6c2a8240812b06e567044c58a8526c5ea8b13
Marco Giering <marco.giering@icloud.com>
2025-06-18 01:11:06 +0200

    first upload

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './Sidebar/header/header';
+import { SectionSidebar } from './Sidebar/section-sidebar/section-sidebar';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, SectionSidebar],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected title = 'JoinDA';
+  protected title = 'join';
 }
