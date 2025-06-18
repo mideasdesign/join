46fabdcfca4a78889b2ea49a1daaa4393f5796bb
Marco Giering <marco.giering@icloud.com>
2025-06-18 15:43:12 +0200

    slider ADD-Card

--- src/app/main-page/main-page.ts
+++ src/app/main-page/main-page.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
-import { Contacts } from '../contacts/contacts';
+import { Component, ViewChild } from '@angular/core';
+import { Contacts } from '../contacts-sektion/contacts/contacts';
+import { AddContacts } from '../contacts-sektion/add-contacts/add-contacts';
 
 @Component({
   selector: 'app-main-page',
-  imports: [Contacts],
+  imports: [Contacts, AddContacts],
   templateUrl: './main-page.html',
   styleUrl: './main-page.scss'
 })
 export class MainPage {
+  @ViewChild(AddContacts) addContactsComponent!: AddContacts;
 
+  openAddContactsPopup(): void {
+    if (this.addContactsComponent) {
+      this.addContactsComponent.openPopup();
+    }
+  }
 }
